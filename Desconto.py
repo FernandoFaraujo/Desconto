e3dacbeb97888e077bc73b70d9a61b33b68180b2
Fernando França Araujo <fernandofaraujo25@gmail.com>
2024-10-13 18:36:31 -0300

    Aplicação de desconto em python

---
+++ Desconto.py
@@ -0,0 +1,7 @@
+valor = float(input("\nDigite aqui seu valor: "))
+
+desconto = float(input("\nDigite aqui o desconto: "))
+
+total = valor - (valor * desconto / 100)
+
+print (f"\nO valor total é de: {total}\n")
